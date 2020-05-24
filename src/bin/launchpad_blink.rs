ff70159da98fdbb7bd84aeeef3c1886e6bef12ca
Matt Mullins <mmullins@mmlx.us>
2020-05-24 00:43:47 -0700

    write out the contents of the SETUP payload
    waiiiiiiiiit a minute I get
    
    	I got a control packet!
    	It is 8 bytes
    	80 06 00 01 00 00 40 00
    	done
    
    which is a GET_DESCRIPTOR request.  Do I already have a device address?
    
    [twenty minutes laterrr]
    
    No, I do not have an address.  The USB 2.0 specification says
    
    	5.  The host assigns a unique address to the USB device, moving the
    	device to the Address state.
    
    	6.  Before the USB device receives a unique address, its Default Control
    	Pipe is still accessible via the default address. The host reads the
    	device descriptor to determine what actual maximum data payload size
    	this USB device’s default pipe can use.
    
    because of course an ordered list is not actually a set of things that
    happen in the order they're listed.

--- src/bin/launchpad_blink.rs
+++ src/bin/launchpad_blink.rs
@@ -76,6 +76,17 @@ pub extern "C" fn stellaris_main() {
         }
 
         writeln!(uart, "I got a control packet!").unwrap();
+
+        let count = (*usb0).count0.read().count().bits();
+        writeln!(uart, "It is {} bytes", count);
+
+        for _ in 0..count {
+            let addr = &(*usb0).fifo0 as *const _ as *const u8;
+            let byte = core::ptr::read_volatile(addr);
+            write!(uart, "{:02x} ", byte);
+        }
+        writeln!(uart).unwrap();
+        writeln!(uart, "done").unwrap();
         // *(0x4005_041c)
 
         loop {}
