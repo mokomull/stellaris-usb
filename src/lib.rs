be719781a81cbe69ca5ae85307f30b7b5b6a1eb0
Matt Mullins <mmullins@mmlx.us>
2020-06-13 18:26:41 -0700

    debug: reinstate the per-poll debugging

--- src/lib.rs
+++ src/lib.rs
@@ -356,29 +356,27 @@ impl<T: Write> usb_device::bus::UsbBus for USB<T> {
 
     fn poll(&self) -> PollResult {
         let res = self.do_poll();
-        // let mut uart = self.uart.borrow_mut();
-        // write!(uart, "poll: ").unwrap();
-        // match res {
-        //     PollResult::None => writeln!(uart, "None"),
-        //     PollResult::Reset => writeln!(uart, "Reset"),
-        //     PollResult::Suspend => writeln!(uart, "Suspend"),
-        //     PollResult::Resume => writeln!(uart, "Resume"),
-        //     PollResult::Data {
-        //         ep_out,
-        //         ep_in_complete,
-        //         ep_setup,
-        //     } => {
-        //         if ep_setup & 0x01 != 0 {
-        //             cortex_m::asm::bkpt();
-        //         }
-        //         writeln!(
-        //             uart,
-        //             "Data {{ setup: {:02x}, tx_complete: {:02x}, rx: {:02x} }}",
-        //             ep_setup, ep_in_complete, ep_out
-        //         )
-        //     }
-        // }
-        // .unwrap();
+        let mut uart = self.uart.borrow_mut();
+        write!(uart, "poll: ").unwrap();
+        match res {
+            PollResult::None => writeln!(uart, "None"),
+            PollResult::Reset => writeln!(uart, "Reset"),
+            PollResult::Suspend => writeln!(uart, "Suspend"),
+            PollResult::Resume => writeln!(uart, "Resume"),
+            PollResult::Data {
+                ep_out,
+                ep_in_complete,
+                ep_setup,
+            } => {
+                cortex_m::asm::bkpt();
+                writeln!(
+                    uart,
+                    "Data {{ setup: {:02x}, tx_complete: {:02x}, rx: {:02x} }}",
+                    ep_setup, ep_in_complete, ep_out
+                )
+            }
+        }
+        .unwrap();
         return res;
     }
 }
